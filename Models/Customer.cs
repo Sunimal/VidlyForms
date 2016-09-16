ceb31db0f31b7e97595fb8b8b1d5114d19e5a87e
Sunimal <ravisherath@gmail.com>
2016-09-16 12:55:03 +1000

    Adding Customer class

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public class Class1
+    class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
